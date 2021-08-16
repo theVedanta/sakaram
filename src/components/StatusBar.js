4323945dc4faed2f519dff368590c41b2f1e9564
mayhixza <emailwithmoi@gmail.com>
2021-08-16 19:36:42 +0530

    fetch

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,10 +1,25 @@
-// import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import {FaCloudShowersHeavy} from 'react-icons/fa'
 import {AiFillClockCircle} from 'react-icons/ai'
 
 import "./StatusBar.css";
 
+let api = `https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=976d88e8699f349aa3435ae965b66663`
+
 function StatusBar() {
+
+  useEffect(() => {
+    fetchFaqs();
+  }, []);
+
+  let [faqs, setFaqs] = useState([]);
+
+  async function fetchFaqs() {
+    const data = await fetch(api);
+    const faqs = await data.json();
+    setFaqs(faqs.faqs);
+  }
+
   return (
     <nav className="status container">
       <div className="status-wrap">
